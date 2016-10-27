6dcb22b22edfcf376155f45549b5af4f790c3f9a
pechentw <pechen.tw@gmail.com>
2016-10-27 19:46:24 +0800

    1st change seek bar

--- app/src/main/java/com/soho/yvtc/yvn2701a/MainActivity.java
+++ app/src/main/java/com/soho/yvtc/yvn2701a/MainActivity.java
@@ -4,17 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import static android.R.attr.start;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar pb;
+    ProgressBar pb, pb2;
+    SeekBar sb;
+    TextView tv2,tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pb = (ProgressBar) findViewById(R.id.progressBar);
+        pb2 = (ProgressBar) findViewById(R.id.progressBar2);
+        sb = (SeekBar) findViewById(R.id.seekBar);
+        tv2 = (TextView) findViewById(R.id.textView2);
+        tv3 = (TextView) findViewById(R.id.textView3);
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv2.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void clickshow(View v) {
@@ -46,5 +71,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }.start();
+    }
+    public void click2(View v)
+    {
+        if (pb2.getProgress() == 100) pb2.setProgress(0);
+        else pb2.setProgress(pb2.getProgress() + 10);
+    }
+    public void click3(View v)
+    {
+        tv3.setText("" + sb.getProgress());
     }
 }
